da1eaf38ed5d7878999f1878010f676e997fcfaa
jonathanzs <jonathanzs@live.com>
2018-06-14 11:47:25 +0800

    erbao-demo V1.0
    简答的页面与交互细节

---
+++ files/填写订单（跳转至开始汇款（已登陆状态））/data.js
@@ -0,0 +1,7 @@
+﻿$axure.loadCurrentPage(
+(function() {
+    var _ = function() { var r={},a=arguments; for(var i=0; i<a.length; i+=2) r[a[i]]=a[i+1]; return r; }
+    var _creator = function() { return _(b,c,d,e,f,g,h,g,i,[j],k,_(l,m,n,o,p,q,r,_(),s,_(t,u,v,w,x,_(y,z,A,B),C,null,D,w,E,w,F,G,H,null,I,J,K,L,M,N,O,J),P,_(),Q,_(R,_(S,T,U,[_(S,V,W,g,X,[_(Y,Z,S,ba,bb,_(bc,k,b,bd,be,bf),bg,bh)])])),bi,_(bj,[])),bk,_(),bl,_());}; 
+var b="url",c="填写订单（跳转至开始汇款（已登陆状态））.html",d="generationDate",e=new Date(1528948000037.92),f="isCanvasEnabled",g=false,h="isAdaptiveEnabled",i="variables",j="OnLoadVariable",k="page",l="packageId",m="03ead2426bb24b188ae2348911c892d6",n="type",o="Axure:Page",p="name",q="填写订单（跳转至开始汇款（已登陆状态））",r="notes",s="style",t="baseStyle",u="627587b6038d43cca051c114ac41ad32",v="pageAlignment",w="near",x="fill",y="fillType",z="solid",A="color",B=0xFFFFFFFF,C="image",D="imageHorizontalAlignment",E="imageVerticalAlignment",F="imageRepeat",G="auto",H="favicon",I="sketchFactor",J="0",K="colorStyle",L="appliedColor",M="fontName",N="Applied Font",O="borderWidth",P="adaptiveStyles",Q="interactionMap",R="onLoad",S="description",T="OnPageLoad",U="cases",V="Case 1",W="isNewIfGroup",X="actions",Y="action",Z="linkWindow",ba="Open 开始汇款 in Current Window",bb="target",bc="targetType",bd="开始汇款.html",be="includeVariables",bf=true,bg="linkType",bh="current",bi="diagram",bj="objects",bk="masters",bl="objectPaths";
+return _creator();
+})());
